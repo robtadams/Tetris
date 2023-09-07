5924aa723bbdbdea112ee1e29d9018025f29996d
Robin Adams <rob.thomas.adams@gmail.com>
2023-09-07 15:46:10 -0700

    Update

--- Tetris.py
+++ Tetris.py
@@ -9,29 +9,64 @@ class Tetris():
 
     def __init__(self):
 
-        self.cellSize = 40
-        self.width = 10 * self.cellSize
-        self.height = 2 * self.width
+        self.cellSize = 25
+        self.width = 15
+        self.height = self.width * 2
 
-        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.testCellX = 5
+        self.testCellY = 5
 
-    def main(self):
+        self.clock = pygame.time.Clock()
+
+        self.running = True
+
+        self.screen = pygame.display.set_mode((self.cellSize * self.width,
+                                                self.cellSize * self.height))
+
+        self.gravityTimerMax = 10
+        self.gravityTimer = self.gravityTimerMax
+
+        self.blockArray = []
 
         for row in range(self.height):
 
-            for cell in range(self.width):
+            temp = []
 
-                if TEST and (row + cell) % 2 == 0:
+            for col in range(self.width):
 
-                    cellRect = [row * self.cellSize, cell * self.cellSize, self.cellSize, self.cellSize]
+                temp.append(0)
 
-                    pygame.draw.rect(self.screen, "white", cellRect)
+            self.blockArray.append(temp)
 
-                    time.sleep(.1)
+        if TEST:
 
-                    pygame.display.update()
+            print("=== Init Values ===")
+            print("Celll Size: {0}\nWidth: {1}\nHeight: {2}".format(self.cellSize, self.width, self.height))
+            print("\nGravity Timer Max: {0}\nGravity Timer: {1}".format(self.gravityTimerMax, self.gravityTimer))
+            print("\nBlock Array: 0 = Empty | 1 = Active | -1 = Static")
+            for row in self.blockArray:
+                for col in row:
+                    print(col, end=" ")
+                print()
 
-myTetris = Tetris()
+    def main(self):
 
-myTetris.main()
-    
+        """ Draw Game Board """
+
+        for y in range(self.width):
+
+            for x in range(self.height):
+
+                outsideRect = [y * self.cellSize, x * self.cellSize,
+                               self.cellSize, self.cellSize]
+
+                insideRect = [y * self.cellSize + 1, x * self.cellSize + 1,
+                              self.cellSize - 2, self.cellSize - 2]
+
+                pygame.draw.rect(self.screen, (25, 25, 25), outsideRect)
+                pygame.draw.rect(self.screen, (0 , 0 , 0 ), insideRect)
+
+        pygame.display.update()
+
+Game = Tetris()
+Game.main()
