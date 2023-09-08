a72b66675bc11ed502741d2ea5d98dc44fa4fe23
Robin Adams <rob.thomas.adams@gmail.com>
2023-09-07 17:51:26 -0700

    Falling Down

--- Pieces.py
+++ Pieces.py
@@ -1,8 +1,82 @@
+import random
+
 class Piece():
 
     def __init__(self):
 
+        """ Initialization """
+
+        # locations: the current location of each segment in the piece
         self.locations = []
+
+        # name: the name of the piece
+        self.name = None
+
+        # remainingShapes: the pieces that have yet to be picked in the set
+        self.remainingShapes = [1, 2, 3, 4, 5, 6, 7]
+
+        # Pick one of the pieces among the list of remaining shapes
+        self.generatePiece()
+
+    def generatePiece(self):
+
+        """ Generate New Piece """
+
+        # If there are no more pieces remaining...
+        if len(self.remainingShapes) <= 0:
+
+            # ... reset the list of remaining pieces
+            self.remainingShapes = [1, 2, 3, 4, 5, 6, 7]
+
+        # Grab a piece from the list of remaining pieces
+        value = random.randint(1, len(self.remainingShapes))
+
+        # Remove the chosen piece from the list of remaining pieces
+        self.remainingShapes.remove(value)
+
+        # Find the piece that was chosen
+        match value:
+
+            # Square
+            case 1:
+                self.Square()
+                self.name = "Square"
+
+            # Line
+            case 2:
+                self.Line()
+                self.name = "Line"
+
+            # L Block Right
+            case 3:
+                self.LBlockRight()
+                self.name = "L Block Right"
+
+            # L Block Left
+            case 4:
+                self.LBlockLeft()
+                self.name = "L Block Left"
+
+            # Z Block Right
+            case 5:
+                self.ZBlockRight()
+                self.name = "Z Block Right"
+
+            # Z Block Left
+            case 6:
+                self.ZBlockLeft()
+                self.name = "Z Block Left"
+
+            # T Block
+            case 7:
+                self.TBlock()
+                self.name = "T Block"
+
+    def fall(self):
+
+        for location in self.locations:
+
+            location[1] += 1
 
     def Square(self):
 
--- Tetris.py
+++ Tetris.py
@@ -38,6 +38,12 @@ class Tetris():
         # gravityTimer: the number of remaining ticks before gravity is applied to the current piece
         self.gravityTimer = self.gravityTimerMax
 
+        # createPiece: whether a piece needs to be created or not
+        self.createPiece = True
+
+        # piece: the piece the player is currently controlling
+        self.piece = None
+
         """ Constructing Block Array """
 
         # blockArray: a 2D array of all the cells in the game
@@ -70,25 +76,139 @@ class Tetris():
                 for col in row:
                     print(col, end=" ")
                 print()
+            print()
 
+    def cleanup(self):
+
+        """ Cleanup """
+
+        # For each row in the block array...
+        for y, row in enumerate(self.blockArray):
+
+            # ... for each column in the row...
+            for x, col in enumerate(row):
+
+                # ... if the cell has an active block in it...
+                if col == 1:
+
+                    # ... remove the active block from the cell
+                    self.blockArray[y][x] = 0
+
+                    # Build the outside (gray) rectangle
+                    outsideX = x * self.cellSize
+                    outsideY = y * self.cellSize
+                    outsideRect = [outsideX, outsideY, self.cellSize, self.cellSize]
+
+                    # Build the inside (black) rectangle
+                    insideX = x * self.cellSize + 1
+                    insideY = y * self.cellSize + 1
+                    insideRect = [insideX, insideY, self.cellSize - 2, self.cellSize - 2]
+
+                    # Draw over the active cell with a grid cell
+                    pygame.draw.rect(self.screen, (25, 25, 25), outsideRect)
+                    pygame.draw.rect(self.screen, (0 , 0 , 0 ), insideRect)
+                    
+
+    def update(self):
+
+        """ Update """
+
+        # For each location in the piece's locations...
+        for location in self.piece.locations:
+
+            # ... get the X and Y coordinates of the cell
+            pieceX = location[0]
+            pieceY = location[1]
+
+            # If the cell is within the boundaries of the game...
+            if pieceX >= 0 and pieceY >= 0:
+
+                # Set the respective cell in the blockArray to 1 (active)
+                self.blockArray[pieceY][pieceX] = 1
+
+                # Get the X and Y coordinates for the cell
+                xCoord = pieceX * self.cellSize
+                yCoord = pieceY * self.cellSize
+                drawRect = [xCoord, yCoord, self.cellSize, self.cellSize]
+
+                # Draw the active cell onto the screen
+                pygame.draw.rect(self.screen, (0, 255, 155), drawRect)
+                
     def main(self):
 
         """ Draw Game Board """
 
+        # For each row in the board...
         for y in range(self.width):
 
+            # ... for each column in the row...
             for x in range(self.height):
 
+                # ... build the outside (gray) square
                 outsideRect = [y * self.cellSize, x * self.cellSize,
                                self.cellSize, self.cellSize]
 
+                # Build the inside (black) square
                 insideRect = [y * self.cellSize + 1, x * self.cellSize + 1,
                               self.cellSize - 2, self.cellSize - 2]
-
+                
                 pygame.draw.rect(self.screen, (25, 25, 25), outsideRect)
                 pygame.draw.rect(self.screen, (0 , 0 , 0 ), insideRect)
 
+        # Update the display
         pygame.display.update()
+
+        """ Main Game Loop """
+
+        # While running is True...
+        while self.running:
+
+            time.sleep(.1)
+
+            # Check if piece needs to be made...
+            if self.createPiece:
+
+                # ... create a new piece
+                self.piece = Piece()
+
+                if TEST:
+                    print("=== New Piece Value ===\nPiece Name: {}".format(self.piece.name))
+                
+                self.createPiece = False
+
+            # Check if piece needs to fall...
+            if self.gravityTimer <= 0:
+
+                # Reset the gravit timer
+                self.gravityTimer = self.gravityTimerMax
+
+                # Remove all 1s (active pieces) from the board
+                self.cleanup()
+
+                # Move the active piece down 1 square
+                self.piece.fall()
+
+                # Update the blockArray with the new piece locations
+                self.update()
+
+                pygame.display.update()
+
+                if TEST:
+                    print("\nBlock Array: 0 = Empty | 1 = Active | -1 = Static")
+                    for row in self.blockArray:
+                        for col in row:
+                            print(col, end=" ")
+                        print()
+                    print()
+
+            else:
+                self.gravityTimer -= 1
+
+            # Check if piece is being moved by the player...
+
+            # Cleanup the screen
+
+            # Draw piece
 
 Game = Tetris()
 Game.main()
